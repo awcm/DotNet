7bdf85124d9bb61b40e3466d1f963270c0f6c2d1
Asim Aryal <46110281+awcm@users.noreply.github.com>
2023-05-31 10:30:54 +0545

    Add files via upload
    Simple Console Program to Roll a dice between two players until one of them reaches 20.

---
+++ C#/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SampleStack
+{
+    class SampleStack
+    {
+        public static void Main(String[] args)
+        {
+            int player1= 0, player2 =0;
+            int rndNum;
+            string winner;
+            Random rnd = new Random();
+
+            
+
+            Console.WriteLine("DICE GAME::First person to 20 wins the games \n");
+
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Rolling dice for the first player");
+                    rndNum = rnd.Next(1, 7);
+                    player1 += rndNum;
+                    Console.WriteLine("The dice rolled to {0} \n", rndNum);
+                    Console.ReadKey();
+                } while (rndNum == 6 || rndNum == 1);
+
+                do
+                {
+                    Console.WriteLine("Rolling dice for the second player");
+                    rndNum = rnd.Next(1, 7);
+                    player2 += rndNum;
+                    Console.WriteLine("The dice rolled to {0}\n", rndNum);
+                    Console.ReadKey();
+                } while (rndNum == 6 || rndNum ==1);
+
+                Console.WriteLine("=================================================");
+                Console.WriteLine($"The score is PLAYER1 : {player1} and PLAYER2 : {player2} \n");
+                Console.WriteLine("=================================================");
+                Console.ReadKey();
+                
+
+
+            } while(player1 < 20 && player2 < 20);
+
+            if(player1 > player2 )
+            {
+                Console.WriteLine("The Winner is Player 1");
+            }else if(player1 < player2)
+            {
+                Console.WriteLine("The Winner is Player 2!!!");
+            }
+            else
+            {
+                Console.WriteLine("Draw!!!");
+            }
+
+            
+
+
+        }
+    }
+}
